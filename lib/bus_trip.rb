78cea5e3382302c8cbe80e0e1b1e9a6758b9ec2f
Derek Kastner <dkastner@gmail.com>
2010-08-12 14:03:38 -0400

    Using emitter gem

--- lib/bus_trip.rb
+++ lib/bus_trip.rb
@@ -1,23 +1,10 @@
+require 'emitter'
+
 module BrighterPlanet
   module BusTrip
-    extend self
+    extend BrighterPlanet::Emitter
 
-    def included(base)
-      require 'cohort_scope'
-      require 'falls_back_on'
-      require 'falls_back_on/active_record_ext'
-
-      require 'bus_trip/carbon_model'
-      require 'bus_trip/characterization'
-      require 'bus_trip/data'
-      require 'bus_trip/summarization'
-
-      base.send :include, BrighterPlanet::BusTrip::CarbonModel
-      base.send :include, BrighterPlanet::BusTrip::Characterization
-      base.send :include, BrighterPlanet::BusTrip::Data
-      base.send :include, BrighterPlanet::BusTrip::Summarization
-    end
-    def bus_trip_model
+    def self.bus_trip_model
       if Object.const_defined? 'BusTrip'
         ::BusTrip
       elsif Object.const_defined? 'BusTripRecord'
