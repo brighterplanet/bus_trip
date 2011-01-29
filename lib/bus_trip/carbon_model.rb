e7a14447db48bcefcd715c69b953a721e0443038
Ian Hough <ian@brighterplanet.com>
2011-01-28 17:33:56 -0800

    Update compliance per DNV

--- lib/bus_trip/carbon_model.rb
+++ lib/bus_trip/carbon_model.rb
@@ -115,11 +115,13 @@ module BrighterPlanet
             end
             
             #### Distance from bus class
-            # **Complies:**
+            # **Complies:** GHG Protocol, ISO 14064-1, Climate Registry Protocol
             #
             # Looks up the [bus class](http://data.brighterplanet.com/bus_classes) `distance` (*km*).
-            quorum 'from bus class', :needs => :bus_class do |characteristics|
-              characteristics[:bus_class].distance
+            quorum 'from bus class',
+              :needs => :bus_class,
+              :complies => [:ghg_protocol, :iso, :tcr] do |characteristics|
+                characteristics[:bus_class].distance
             end
           end
           
