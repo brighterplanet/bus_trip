165ed36f9942dccd1af221a469b5f8b26a8a8772
Seamus Abshere <seamus@abshere.net>
2011-10-22 17:44:04 -0500

    foreign key name

--- lib/bus_trip/relationships.rb
+++ lib/bus_trip/relationships.rb
@@ -2,7 +2,7 @@ module BrighterPlanet
   module BusTrip
     module Relationships
       def self.included(target)
-        target.belongs_to :bus_class
+        target.belongs_to :bus_class, :foreign_key => 'bus_class_name'
       end
     end
   end
